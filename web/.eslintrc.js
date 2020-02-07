5640ec0a4deb0ac344f9cbe68ec32a5ceddca1bb
Sevi <sevi.muelli@gmail.com>
2020-02-07 19:07:33 +0100

    Add eslint-plugin-react-hooks and modify eslintrc.js

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
+  'plugins': ['react-hooks'],
   rules: {
     'react/prop-types': 0,
-    'object-curly-spacing': ['error', 'never']
+    'object-curly-spacing': ['error', 'never'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
   settings: {
     react: {
